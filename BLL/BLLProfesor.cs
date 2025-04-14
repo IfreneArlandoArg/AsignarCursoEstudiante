c3c9e72fc85bd66b865197d1761c28d34803a1c5
Arlando Ifrene <aifrene@quadiontech.com>
2025-04-14 11:03:41 -0300

    Basic BLL, DAL setup for Profesor

--- BLL/BLLProfesor.cs
+++ BLL/BLLProfesor.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BE;
+using DAL;
+
+namespace BLL
+{
+    public class BLLProfesor : IABML<BEProfesor>
+    {
+        public void Alta(BEProfesor pT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AltaCurso(BEProfesor pT, BECurso pC)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Baja(BEProfesor pT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BEProfesor> Listar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<BEProfesor> ListarCursos(BEProfesor pT)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Modificar(BEProfesor pT)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
